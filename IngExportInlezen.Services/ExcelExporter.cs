970596086f0e598b6b1d0a8b4d11ebfd65b4452a
coenj <coenj@LAPTOP-LS143VD9>
2024-01-04 21:34:12 +0100

    kleur

--- IngExportInlezen.Services/ExcelExporter.cs
+++ IngExportInlezen.Services/ExcelExporter.cs
@@ -326,7 +326,7 @@ namespace IngExportInlezen.Services
             cell2.Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
             if ((double)sumBedrag > com)
             {
-                cell2.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.FromArgb(255, 255, 102));
+                cell2.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.FromArgb(252, 196, 94));
             }
             else
             {
